8993f4a23e5d304f8f0b1a5b3bd658d8fbbb5425
Tomáš Kelbasa <91247802+TomasKelbasa@users.noreply.github.com>
2023-05-14 11:51:30 +0200

    tohle jsem určitě dělal já a ne chatGPT

--- main.ts
+++ main.ts
@@ -1,6 +1,11 @@
-// knihovna.ts
+/**
+ * Tato knihovna poskytuje třídu SmoothServo, která umožňuje ovládat servo motor na mikrokontroléru micro:bit.
+ */
 namespace smoothServo {
 
+    /**
+     * Třída SmoothServo umožňuje ovládat servo motor pomocí plynulých pohybů.
+     */
     export class SmoothServo {
         private pin: AnalogPin;
 
@@ -11,6 +16,10 @@ namespace smoothServo {
         private min: number = 400;
         private max: number = 2600;
 
+        /**
+         * Konstruktor třídy SmoothServo.
+         * @param pin Analogový vstup, ke kterému je připojen servo motor.
+         */
         constructor(pin: AnalogPin) {
             this.pin = pin;
             this.target = 0;
@@ -18,6 +27,11 @@ namespace smoothServo {
             pins.servoSetPulse(this.pin, this.current);
         }
 
+        /**
+         * Pohne servo motorem na zadanou pozici.
+         * @param position Pozice, na kterou se má servo motor posunout (v rozsahu od 0 do 180).
+         * @param speed Rychlost pohybu motoru (v rozsahu od 1 do 100).
+         */
         public moveTo(position: number, speed: number = 30) {
             this.target = position;
             let step = speed;
@@ -32,6 +46,9 @@ namespace smoothServo {
             }
         }
 
+        /**
+         * Aktualizuje pozici servo motoru.
+         */
         public update() {
             if (this.zasobnik !== undefined && this.zasobnik.length > 0) {
                 let e = this.zasobnik.shift()
@@ -43,7 +60,11 @@ namespace smoothServo {
         }
     }
 
-    // vytvoří nový SmoothServo
+    /**
+     * Vytvoří nový SmoothServo objekt pro ovládání servo motoru.
+     * @param pin Analogový vstup, ke kterému je připojen servo motor.
+     * @returns Nový SmoothServo objekt.
+     */
     export function createServo(pin: AnalogPin): SmoothServo {
         return new SmoothServo(pin);
     }
